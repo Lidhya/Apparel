5b431a609611de0873a7f70648d5452d8324adfc
Lidhya <lidhyajeejan@gmail.com>
2022-09-11 14:26:53 +0530

    final-draft

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mongoClient=require('mongodb').MongoClient
 const state={ db:null}
 
-
+/* ---------------------------- connect to cloud db ---------------------------- */
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
+    const url='mongodb+srv://lidhyajeejan:bSUnAdS45xPLKgw3@cluster0.7lsdxpb.mongodb.net/?retryWrites=true&w=majority'
     const dbname='Apparel'
 
     mongoClient.connect(url,(err,data)=>{
@@ -12,6 +12,18 @@ module.exports.connect=function(done){
         done()
     })
 }
+
+/* --------------------------- connect to local db -------------------------- */
+// module.exports.connect=function(done){
+//     const url='mongodb://localhost:27017'
+//     const dbname='Apparel'
+
+//     mongoClient.connect(url,(err,data)=>{
+//         if(err) return done(err)
+//         state.db=data.db(dbname)
+//         done()
+//     })
+// }
 
 module.exports.get=function(){
     return state.db
